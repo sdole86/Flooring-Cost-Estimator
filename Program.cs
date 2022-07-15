a3e2a736622a97453feb5bbf4fb3e1b0be221597
sdole86 <92174592+sdole86@users.noreply.github.com>
2022-07-14 21:55:39 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Flooring_Cost_Estimator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double CostPerFoot = 35.75;
+            const double FeetPerHour = 40;
+        
+            Console.WriteLine("Samuel Dole's Flooring Cost Estimator!");
+            Console.WriteLine();
+            Console.Write("Please enter length of floor: ");
+            int lengthFloor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter width of floor: ");
+            int widthFloor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the cost per square foot for the flooring selected: ");
+            double costFloor = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("------------------------");
+
+            int totalFloor = lengthFloor * widthFloor;
+            double floorCost = Convert.ToDouble(totalFloor) * costFloor;
+            Console.WriteLine($"For a total floor size of {totalFloor} the floor cost is: {floorCost.ToString("C")}");
+
+            double totalHours = Convert.ToDouble(totalFloor) / FeetPerHour;
+            double installCost = totalHours * CostPerFoot;
+            double totalCost = installCost + floorCost;
+            Console.WriteLine($"It will take {totalHours} hours to install the floor at a cost of {installCost.ToString("C")}");
+            Console.WriteLine();
+            Console.WriteLine($"The total finished cost for the new floor is: {totalCost.ToString("C")}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Flooring-Cost-Estimator
+ Console app that determines cost of flooring based on width, length, and cost
